6ee57fda26a7c79efa7c581cf211820f7b6515f2
kil <godngu@gmail.com>
2021-02-21 20:49:58 +0900

    엔티티 생성: member, team

---
+++ src/test/java/study/querydsl/entity/MemberTest.java
@@ -0,0 +1,57 @@
+package study.querydsl.entity;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class MemberTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    void testEntity() {
+        // given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        persistTeams(teamA, teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        persistMembers(member1, member2, member3, member4);
+
+        em.flush();
+        em.clear();
+
+        // when
+        List<Member> members = em.createQuery("select m from Member m", Member.class)
+            .getResultList();
+
+        // then
+        for (Member member : members) {
+            System.out.println("member = " + member);
+            System.out.println("-> member.team=" + member.getTeam());
+        }
+    }
+
+    private void persistMembers(Member... members) {
+        for (Member member : members) {
+            em.persist(member);
+        }
+    }
+
+    private void persistTeams(Team... teams) {
+        for (Team team : teams) {
+            em.persist(team);
+        }
+    }
+
+
+}
