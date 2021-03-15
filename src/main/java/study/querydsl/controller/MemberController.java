f25990edd659c910e60d96caaac57e902a21343b
kil <godngu@gmail.com>
2021-03-15 14:57:36 +0900

    컨트롤러 페이징 적용

--- src/main/java/study/querydsl/controller/MemberController.java
+++ src/main/java/study/querydsl/controller/MemberController.java
@@ -2,20 +2,40 @@ package study.querydsl.controller;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.repository.MemberJpaRepository;
+import study.querydsl.repository.MemberRepository;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberJpaRepository memberJpaRepository;
+    private final MemberRepository memberRepository;
 
     @GetMapping("/v1/members")
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
         return memberJpaRepository.search(condition);
+    }
+
+    @GetMapping("/v2/members")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageSimple(condition, pageable);
+    }
+
+    /**
+     * 테스트 데이터의 총 개수는 100개, size를 110으로 던지면 totalCount 쿼리는 발생하지 않는다!! 페이징 최적화!
+     * @param condition
+     * @param pageable
+     * @return
+     */
+    @GetMapping("/v3/members")
+    public Page<MemberTeamDto> searchMemberV3(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageComplex(condition, pageable);
     }
 }
